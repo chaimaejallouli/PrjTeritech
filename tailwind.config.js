2c1b5a8b730354ca7279c0a2dc2194e2072ea507
jallouli chaimae <cjallouli10@gmail.com>
2024-04-05 10:17:27 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+
